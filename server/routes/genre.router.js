4f5823b14a38ee69f0de603df473f7d5d2da4d36
J Read <j@8bit.coffee>
2023-12-05 16:28:25 -0600

    genre GET and storage complete

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+  let queryText = `SELECT * FROM "genres";`
+  pool.query(queryText).then((result) =>{
+    res.send(result.rows);
+  })
+  .catch((error) => {
+    console.error(`Error in GET '/genre'.`, error);
+    res.sendStatus(500);
+  })
+  
 });
 
 module.exports = router;
