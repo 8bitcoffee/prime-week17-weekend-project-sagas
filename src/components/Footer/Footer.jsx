30331e237a77ab22ed1dbe0b5c8d3f839afef39a
J Read <j@8bit.coffee>
2023-12-06 21:27:06 -0600

    added CSS and MUI sx styling to details page. Optimized layout for variable screen sizes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,15 @@ import './Footer.css';
 function Footer(){
     return (
         <div id="footer-div">
-            <h6><img id="footer-logo" src='images/8bitlogo.png' alt='8bit logo'/>8bitcoffee - 2023</h6>
+            <a href='https://github.com/8bitcoffee'>
+                <h6>
+                    <img 
+                        id="footer-logo"
+                        src='images/8bitlogo.png'
+                        alt='8bit logo'
+                    />
+                8bitcoffee - 2023</h6>
+            </a>
         </div>
     )
 }
